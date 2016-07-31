9e40cfb672465050dfd167e12659b874ceb65f46
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-31 05:44:50 -0400

    generate project trees

--- generator.js
+++ generator.js
@@ -1,18 +1,38 @@
 'use strict';
 
 var path = require('path');
+var util = require('util');
+var clone = require('clone-deep');
 var tree = require('base-fs-tree');
 var isValid = require('is-valid-app');
-var src = path.resolve.bind(path, __dirname, 'templates');
+var compare = require('./compare');
 
-module.exports = function(app, base) {
+module.exports = function(app) {
   if (!isValid(app, 'generate-project')) return;
+  var trees = {};
 
   /**
    * Plugins
    */
 
-  app.use(tree({name: 'generate-project'}));
+  app.use(tree({name: 'generate-project', write: false}));
+  app.option('treename', function(file) {
+    file.stem = file.stem.replace(/generate-project-/, '');
+  });
+
+  app.preWrite(/./, function(file, next) {
+    if (app.options.tree && !/tree/.test(file.path)) {
+      file.contents = null;
+    }
+    next();
+  });
+
+  // app.on('tree', function(namespace, name, prop, tree) {
+  //   if (prop === 'dest') {
+  //     trees[name] = clone(Object.keys(tree[namespace][prop].cwd));
+  //   }
+  // });
+
   app.use(require('generate-defaults'));
   app.use(require('generate-install'));
 
@@ -20,12 +40,13 @@ module.exports = function(app, base) {
    * Micro-generator plugins
    */
 
-  app.use(require('generate-package'));
+  app.use(require('generate-contributing'));
   app.use(require('generate-editorconfig'));
   app.use(require('generate-eslint'));
   app.use(require('generate-gitattributes'));
   app.use(require('generate-gitignore'));
   app.use(require('generate-license'));
+  app.use(require('generate-package'));
   app.use(require('generate-readme'));
   app.use(require('generate-travis'));
 
@@ -34,21 +55,48 @@ module.exports = function(app, base) {
    * task).
    *
    * ```sh
-   * $ gen files
+   * $ gen project:files
    * ```
    * @name files
    * @api public
    */
 
-  app.task('files', [
-    'package',
-    'readme',
-    'mit',
+  app.task('files', ['dotfiles', 'rootfiles']);
+
+  /**
+   * Generate the dotfiles from registered micro-generators .
+   *
+   * ```sh
+   * $ gen project:dotfiles
+   * ```
+   * @name dotfiles
+   * @api public
+   */
+
+  app.task('dotfiles', [
     'editorconfig',
     'eslintrc',
     'gitattributes',
     'gitignore',
     'travis'
+  ]);
+
+  /**
+   * Generate the main project files from registered micro-generators: `contributing.md`,
+   * `LICENSE`, 'package.json' and `README.md`.
+   *
+   * ```sh
+   * $ gen project:rootfiles
+   * ```
+   * @name rootfiles
+   * @api public
+   */
+
+  app.task('rootfiles', [
+    'contributing',
+    'mit',
+    'package',
+    'readme'
   ]);
 
   /**
@@ -65,7 +113,7 @@ module.exports = function(app, base) {
    * @api public
    */
 
-  app.task('project', ['files', 'index']);
+  app.task('project', ['dotfiles', 'index', 'rootfiles']);
 
   /**
    * Scaffold out basic project for a [gulp][] plugin.
@@ -77,9 +125,22 @@ module.exports = function(app, base) {
    * @api public
    */
 
-  app.task('gulp', ['gf', 'gp', 'files']);
+  app.task('gulp', ['dotfiles', 'gp', 'gf', 'rootfiles']);
   task(app, 'gf', 'gulp/gulpfile.js');
   task(app, 'gp', 'gulp/plugin.js');
+
+  /**
+   * Scaffold out a [base][] plugin project.
+   *
+   * ```sh
+   * $ gen project:base
+   * ```
+   * @name base
+   * @api public
+   */
+
+  app.task('base', ['dotfiles', 'base-index', 'rootfiles']);
+  task(app, 'base-index', 'base/plugin.js');
 
   /**
    * Scaffold out a minimal code project,
@@ -94,22 +155,21 @@ module.exports = function(app, base) {
    */
 
   app.task('min', ['minimal']);
-  app.task('minimal', ['package', 'readme', 'mit', 'gitignore', 'gitattributes']);
+  app.task('minimal', ['gitignore', 'mit', 'package', 'readme']);
 
   /**
    * Scaffold out a basic [generate][] generator project.
    *
    * ```sh
    * $ gen project:generator
-   * # also aliased as
-   * $ gen project:gen
    * ```
    * @name generator
    * @api public
    */
 
   app.task('gen', ['generator']);
-  task(app, 'generator', 'generator/*.js', ['files']);
+  app.task('generator', ['dotfiles', 'generator-files', 'rootfiles']);
+  task(app, 'generator-files', 'generator/*.js');
 
   /**
    * Scaffold out a basic template helper project.
@@ -147,6 +207,25 @@ module.exports = function(app, base) {
    */
 
   app.task('default', ['project']);
+
+  /**
+   * Generate project trees
+   */
+
+  app.task('tree-default', ['default'], createTree(app));
+  app.task('tree-dotfiles', ['dotfiles'], createTree(app));
+  app.task('tree-generator', ['generator'], createTree(app));
+  app.task('tree-gulp', ['gulp'], createTree(app));
+  app.task('tree-min', ['min'], createTree(app));
+  app.task('tree-minimal', ['minimal'], createTree(app));
+  app.task('tree-project', ['project'], createTree(app));
+  app.task('tree-rootfiles', ['rootfiles'], createTree(app));
+  app.task('trees', ['tree-*', 'compare-trees']);
+  app.task('compare-trees', function(cb) {
+    compare(app.trees.views);
+    // console.log();
+    cb();
+  });
 }
 
 /**
@@ -155,9 +234,23 @@ module.exports = function(app, base) {
 
 function task(app, name, pattern, dependencies) {
   app.task(name, dependencies || [], function(cb) {
-    return app.src(pattern, {cwd: src()})
-      .pipe(app.renderFile('*'))
-      .pipe(app.conflicts(app.cwd))
-      .pipe(app.dest(app.cwd));
+    return file(app, pattern);
   });
+}
+
+function file(app, pattern) {
+  var src = app.options.srcBase || path.join(__dirname, 'templates');
+  return app.src(pattern, {cwd: src})
+    .pipe(app.renderFile('*')).on('error', console.log)
+    .pipe(app.conflicts(app.cwd))
+    .pipe(app.dest(app.cwd));
+}
+
+function createTree(app) {
+  return function(cb) {
+    var dest = app.options.trees || path.join(app.cwd, 'trees');
+    var name = this.name.replace(/^tree-/, '');
+    app.createTrees({name: name, dest: dest});
+    cb();
+  }
 }
--- test/test.js
+++ test/test.js
@@ -12,6 +12,7 @@ var pkg = require('../package');
 var generator = require('..');
 var app;
 
+var cwd = path.resolve.bind(path, process.cwd());
 var actual = path.resolve.bind(path, __dirname, 'actual');
 
 function exists(name, cb) {
@@ -21,7 +22,7 @@ function exists(name, cb) {
     fs.stat(filepath, function(err, stat) {
       if (err) return cb(err);
       assert(stat);
-      del(actual(), cb);
+      cb();
     });
   };
 }
@@ -36,13 +37,22 @@ describe('generate-project', function() {
   }
 
   before(function(cb) {
-    del(actual(), cb);
+    del([cwd('test/trees'), actual()], cb);
   });
 
-  beforeEach(function(cb) {
+  // after(function(cb) {
+  //   del(actual(), cb);
+  // });
+
+  beforeEach(function() {
     app = generate({silent: true});
+
     app.cwd = actual();
     app.option('dest', actual());
+    app.option('trees', cwd('test/trees'));
+    app.option('overwrite', function(file) {
+      return /actual/.test(file.path);
+    });
 
     // pre-populate template data to avoid prompts from `ask` helper
     app.option('askWhen', 'not-answered');
@@ -62,11 +72,6 @@ describe('generate-project', function() {
         version: '0.1.0'
       }
     });
-    del(actual('*'), cb);
-  });
-
-  afterEach(function(cb) {
-    del(actual('*'), cb);
   });
 
   describe('tasks', function() {
@@ -78,12 +83,120 @@ describe('generate-project', function() {
 
     it('should run the `default` task with .build', function(cb) {
       app.use(generator);
-      app.build('default', exists('package.json', cb));
+      app.build(['default'], exists('package.json', cb));
     });
 
     it('should run the `default` task with .generate', function(cb) {
       app.use(generator);
       app.generate('default', exists('package.json', cb));
+    });
+  });
+
+  describe('files', function() {
+    beforeEach(function() {
+      app.cwd = actual();
+    });
+
+    it('should run the `gitignore` task', function(cb) {
+      app.register('project', generator);
+      app.generate('project:gitignore', exists('.gitignore', cb));
+    });
+
+    it('should generate a LICENSE file', function(cb) {
+      app.register('project', generator);
+      app.generate('project:mit', exists('LICENSE', cb));
+    });
+
+    it('should generate an index.js file', function(cb) {
+      app.register('project', generator);
+      app.generate('project:index', exists('index.js', cb));
+    });
+
+    it('should generate a .eslintrc.json file', function(cb) {
+      app.register('project', generator);
+      app.generate('project:eslintrc', exists('.eslintrc.json', cb));
+    });
+
+    it('should generate a README.md file', function(cb) {
+      app.register('project', generator);
+      app.generate('project:readme', exists('README.md', cb));
+    });
+
+    it('should generate a package.json file', function(cb) {
+      app.register('project', generator);
+      app.generate('project:package', exists('package.json', cb));
+    });
+
+    it('should generate a .gitignore file', function(cb) {
+      app.register('project', generator);
+      app.generate('project:gitignore', exists('.gitignore', cb));
+    });
+
+    it('should generate a .gitattributes file', function(cb) {
+      app.register('project', generator);
+      app.generate('project:gitattributes', exists('.gitattributes', cb));
+    });
+
+    it('should generate a .editorconfig file', function(cb) {
+      app.register('project', generator);
+      app.generate('project:editorconfig', exists('.editorconfig', cb));
+    });
+
+    it.only('should generate dotfiles', function(cb) {
+      app.register('project', generator);
+      app.generate('project:dotfiles', exists('.editorconfig', cb));
+    });
+  });
+
+  describe('trees', function() {
+    beforeEach(function() {
+      app.cwd = actual();
+    });
+
+    it('should generate trees for all of the tasks', function(cb) {
+      app.enable('overwrite');
+      app.register('project', generator);
+      app.generate('project:trees', cb);
+    });
+
+    it('should generate a tree for the default task', function(cb) {
+      app.register('project', generator);
+      app.generate('project:tree-default', exists('../trees/default-dest.txt', cb));
+    });
+
+    it('should generate a tree for the dotfiles task', function(cb) {
+      app.register('project', generator);
+      app.generate('project:tree-dotfiles', exists('../trees/dotfiles-dest.txt', cb));
+    });
+
+    it('should generate a tree for the generator task', function(cb) {
+      app.register('project', generator);
+      app.generate('project:tree-generator', exists('../trees/generator-dest.txt', cb));
+    });
+
+    it('should generate a tree for the gulp task', function(cb) {
+      app.register('project', generator);
+      app.generate('project:tree-gulp', exists('../trees/gulp-dest.txt', cb));
+    });
+
+    it('should generate a tree for the minimal task', function(cb) {
+      app.register('project', generator);
+      app.generate('project:tree-minimal', exists('../trees/minimal-dest.txt', cb));
+    });
+
+    it('should generate a tree for the min task alias', function(cb) {
+      app.register('project', generator);
+      app.generate('project:tree-min', exists('../trees/min-dest.txt', cb));
+    });
+
+    it('should generate a tree for the project task', function(cb) {
+      app.register('project', generator);
+      app.generate('project:tree-project', exists('../trees/project-dest.txt', cb));
+    });
+
+    it('should generate a tree for the rootfiles task', function(cb) {
+      app.register('project', generator);
+      app.generate('project:tree-rootfiles', exists('../trees/rootfiles-dest.txt', cb));
     });
   });
 
@@ -125,6 +238,13 @@ describe('generate-project', function() {
     it('should run the `min` alias task', function(cb) {
       app.register('project', generator);
       app.generate('project:min', exists('package.json', cb));
+    });
+  });
+
+  describe('project:gulp', function() {
+    it('should run the gulp task on the generator', function(cb) {
+      app.register('project', generator);
+      app.generate('project:gulp', exists('gulpfile.js', cb));
     });
   });
 
